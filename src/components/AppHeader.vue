32e27bdc3ea9e0169220fc278dbca2be9316adb2
GiPisano <pisanogiacomo9@gmail.com>
2024-11-28 21:37:07 +0100

    fix header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,112 +1,114 @@
 <script>
 export default {
-    props:{
-        links: Array, 
-        iconHeader: Array,
+  props: {
+    links: Array,
+    iconHeader: Array,
+  },
+
+  methods: {
+    activeGenders(index) {
+      this.links.forEach((link, i) => {
+        if (i == index) {
+          link.active = true;
+        } else {
+          link.active = false;
+        }
+      });
     },
 
-    methods:{
-        activeGenders(index) {
-            this.links.forEach((link, i) => {
-                if (i == index) {
-                    link.active = true;
-                } else {
-                    link.active = false;
-                }
-            });
-        },
-        
-        activeIconLink(index) {
-            this.links.forEach((iconHeader, i) => {
-                if (i == index) {
-                    iconHeader.active = true;
-                } else {
-                    iconHeader.active = false;
-                }
-            });
-        },
-    }
-}
+    activeIconLink(index) {
+      this.iconHeader.forEach((icon, i) => {
+        if (i == index) {
+          icon.active = true;
+        } else {
+          icon.active = false;
+        }
+      });
+    },
+  },
+};
 </script>
 
 <template>
-    <header>
+  <header>
+    <div>
+      <ul>
+        <li
+          v-for="(link, index) in links"
+          :class="link.active ? 'active' : ''"
+          @click="activeGenders(index)"
+        >
+          <a href="#">{{ link.geneder }}</a>
+        </li>
+      </ul>
+    </div>
+    <div class="logo">
+      <img src="../assets/img/boolean-logo.png" alt="" />
+    </div>
 
-        <div>
-            <ul>
-                <li v-for="(link, index) in links" :class="link.active ? 'active' : ''" @click="activeGenders(index)">
-                    <a href="#">{{ link.geneder }}</a>
-                </li>
-            </ul>
-        </div>
-        <div class="logo">
-            <img src='../assets/img/boolean-logo.png' alt="">
-        </div>
-        
-        <div class="icon" >
-            <i v-for="(icon, index) in iconHeader"
-            @click="activeIconLink(index)" 
-            :class="icon.class, icon.active ? 'active' : ''"></i>
-        </div>
-    
-    </header>
-    </template>
+    <div class="icon">
+      <i
+        v-for="(icon, index) in iconHeader"
+        @click="activeIconLink(index)"
+        :class="[icon.class, icon.active ? 'active' : '']"
+      ></i>
+    </div>
+  </header>
+</template>
 
 <style lang="scss" scoped>
-@use '../styles/partials/mixins.scss' as *;
+@use "../styles/partials/mixins.scss" as *;
 
-header{
-    @include flex();
-    padding: 10px 17%;
-    background-color: rgb(255, 104, 1);
-    color: white;
-    position: fixed;
-    top: 0;
-    right: 0;
-    left: 0;
-    z-index: 1;
+header {
+  @include flex();
+  padding: 10px 17%;
+  background-color: rgb(255, 104, 1);
+  color: white;
+  position: fixed;
+  top: 0;
+  right: 0;
+  left: 0;
+  z-index: 1;
 }
 
-.logo{
-    min-width: 100px;
-    max-width: 100px
-};
+.logo {
+  min-width: 100px;
+  max-width: 100px;
+}
 
-ul{
-    display: flex;
+ul {
+  display: flex;
+  gap: 10px;
+
+  li {
+    list-style-type: none;
+    padding: 5px;
+    a {
+      text-decoration: none;
+      color: inherit;
+    }
+  }
+}
+
+.active {
+  color: black;
+}
+
+.icon {
+  i {
+    padding: 5px;
+    cursor: pointer;
+
+    &.activeIcon {
+      color: black;
+    }
+  }
+}
+
+@media screen and (max-width: 550px) {
+  header {
+    flex-direction: column;
     gap: 10px;
-
-    li{
-        list-style-type: none;
-        padding: 5px;
-        a{
-            text-decoration: none;
-            color: inherit;
-        }
-    }
+  }
 }
-
-.active{
-    color: black;
-}
-
-.icon{
-    i{
-        padding: 5px;
-        cursor: pointer;
-
-        &.activeIcon{
-            color: black;
-        }
-    }
-
-}
-
-@media screen and (max-width: 550px){
-        header{
-           flex-direction: column;
-           gap: 10px;
-        }
-    }
-
 </style>
